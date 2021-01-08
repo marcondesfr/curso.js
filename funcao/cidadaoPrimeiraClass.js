0ab8b86c137668b74ca2273287748907d88c1704
Marcondes <marcondes@localhost.localdomain>
2021-01-08 19:09:46 -0300

    Add Aula de Functions

---
+++ funcao/cidadaoPrimeiraClass.js
@@ -0,0 +1,33 @@
+// Funcao em JS e First-Class Object (Citizens)
+// Higher-order function
+
+// criar de forma literal
+function fun1() { }
+
+// Armazenar em uma variavel
+const fun2 = function () { }
+
+// Armazenar em uma array
+const array =  [function (a,b) { return a + b}, fun1, fun2]
+console.log(array[0] (2,3))
+
+// Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function () {
+    return 'Opa'
+}
+console.log(obj.falar())
+
+// Passar funcao como parametro
+function run(fun) {
+    fun()
+}
+
+run(function () { console.log('Executando...') })
+
+// Uma funcao pode retornar/conter um funcao
+function soma(a, b) {
+    return function (c) {
+        console.log(a + b + c)
+    }
+}
