87f349c1391d3fe040052361235bcec4ead09db2
Marcondes Ferreira Pinheiro <marcondesfr@localhost.localdomain>
2021-04-13 15:43:46 -0400

    NODE

---
+++ Node/node.js
@@ -0,0 +1,10 @@
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log(moduloB.ola)
+console.log(moduloB.bemVindo)
+console.log(moduloB.ateLogo)
+
+
+console.log(moduloA.bomDia)
+console.log(moduloA.bomNoite())
