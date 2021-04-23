87cfd631af5a8d792664d8bf6f2bbc05658ffa60
Marcondes Ferreira Pinheiro <marcondesfr@localhost.localdomain>
2021-04-23 17:23:07 -0400

    bancoDeDados.js add

---
+++ Node/projeto/src/bancoDeDados.js
@@ -0,0 +1,22 @@
+const sequence = {
+    _id: 1,
+    get id() { return this._id++ }
+}
+
+const produtos = {}
+
+function salvarProtudo() {
+    if (!produto.id) produto.id = sequence.id // Se o produto nao estiver setado, produto.id vai receber sequence.id
+    produtos[produto.id] = produto // Dentro de produtos, sera setado id de protudo, e se ja estiver setado, sera renovado
+    return produto // Retorna o produto com id
+}
+
+function getProduto(id) { // Pegar Produto por id
+    return produtos[id] || {} // Caso nao tenha um id de protudo voltar um obj vazio
+}
+
+function getProdutos() { // Pegar todos os Produtos
+    return Object.values(produtos) // Retorna apenas os valores dos produtos
+}
+
+module.exports = { salvarProtudo, getProduto, getProdutos}
