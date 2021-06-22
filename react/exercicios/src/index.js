7941f59c638a97e88c31ebfbb28509bd272e412a
marcondesfr <marcondessam@gmail.com>
2021-06-22 19:50:19 -0400

    React-Calculator

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import BomDia from './components/BomDia'
+//import BomDia from './components/BomDia'
+//import { BoaTarde, BoaNoite } from './components/Multiplos'
+//import Saudacoes from './components/Saudacoes'
+import Pai from './components/Pai'
+import Filho from './components/Filho'
 
-ReactDOM.render(< BomDia nome="Marcondes"/> , document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <Pai nome="Paulo" sobrenome="Silva">
+            {/* Como passo os componentes Filhos aqui? */}
+            <Filho nome="Pedro" sobrenome="Silva"/>
+            <Filho nome="Paulo" sobrenome="Silva"/>
+            <Filho nome="Carla" sobrenome="Silva"/>
+            </Pai>
+    </div> , document.getElementById('root'))
