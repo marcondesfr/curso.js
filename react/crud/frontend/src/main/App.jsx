d39b1cb8f0a4e3fd75e72ee2a4dce5d315387a77
marcondesfr <marcondessam@gmail.com>
2021-06-24 18:25:44 -0400

    react-form2

--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
-import React from './react'
-import Logo from '../components/templats/Header'
+import React from 'react'
+import Logo from '../components/templats/Logo'
 import Nav from '../components/templats/Nav'
 import Main from '../components/templats/Main'
 import Footer from '../components/templats/Footer'
