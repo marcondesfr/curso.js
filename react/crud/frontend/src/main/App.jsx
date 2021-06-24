93cda43e6966f9bf436f08b4444f4b89f53db016
marcondesfr <marcondessam@gmail.com>
2021-06-24 17:46:02 -0400

    react-forms

---
+++ react/crud/frontend/src/main/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import React from './react'
+import Logo from '../components/templats/Header'
+import Nav from '../components/templats/Nav'
+import Main from '../components/templats/Main'
+import Footer from '../components/templats/Footer'
+
+
+
+
+export default props => 
+    <div className="app">
+        <Logo />
+        <Nav />
+        <Main />
+        <Footer />
+    </div>
