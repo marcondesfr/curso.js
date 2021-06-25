801a147958a710e91907cc8531f605c0ba6ed225
marcondesfr <marcondessam@gmail.com>
2021-06-25 18:56:58 -0400

    form-react-3

--- react/crud/frontend/src/main/App.jsx
+++ react/crud/frontend/src/main/App.jsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
 import Logo from '../components/templats/Logo'
@@ -12,6 +14,6 @@ export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main />
+        <Main icon="home" title="Inicio" subtitle="Segundo Projeto do capitulo de React." />
         <Footer />
     </div>
