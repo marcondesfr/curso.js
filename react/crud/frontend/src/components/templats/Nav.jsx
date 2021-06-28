6bc62a069e86eb6daca7803fcb59bb62f0ca4a4e
marcondesfr <marcondessam@gmail.com>
2021-06-28 16:46:29 -0400

    react-+1

--- react/crud/frontend/src/components/templats/Nav.jsx
+++ react/crud/frontend/src/components/templats/Nav.jsx
@@ -3,5 +3,12 @@ import React from 'react'
 
 export default props => 
     <aside className="menu-area">
-        Navegacao
+        <nav className="menu">
+            <a href="#/">
+                <i className="fa fa-home"></i> Inicio
+            </a>
+            <a href="#/">
+                <i className="fa fa-users"></i> Usuarios
+            </a>
+        </nav>
     </aside>
